c5be4d13e5190cb0348f7f296646fb6432647862
Jay <63109802+bjw1622@users.noreply.github.com>
2022-01-11 16:53:12 +0900

    메뉴 삭제 구현

--- src/js/index.js
+++ src/js/index.js
@@ -17,11 +17,10 @@
 // - [] 메뉴의 삭제는 button click event를 받고, 메뉴 삭제 confirm 모달창이 뜬다.
 // - [] 확인 버튼을 클릭하면 메뉴가 삭제된다.
 // - [] 삭제 처리 후 총 메뉴 갯수를 count하여 상단에 보여준다. 요구사항 분석의 중요성을 알고 앞으로 프로그램을 만들기 전 항상
+
 // 요구사항 분석 철저하게 하기! 요구사항 분석이 왜 중요한가?? 목적을 명확하게 체크하지 않으면 길을 해맨다. 상세하게 기재하면 모르는 부분
 // 막히는 부분을 알고 쉽게 해쳐나갈 수 있다. 모르는 부분은 기능 구현을 하면서 학습하면서 해결할 수 있다. 필요한 지식을 빠르게 파악하고
-// 얻을 수 있다. const $ = (selector) => document.querySelector(selector); const $ =
-// function(selector){     return document.querySelector(selector); }
-
+// 얻을 수 있다.
 
 const $ = (selector) => document.querySelector(selector);
 
@@ -67,6 +66,8 @@ function App() {
   // TODO 메뉴 수정
   // - [x] 메뉴의 수정은 button click event를 받고, 메뉴 수정하는 모달창(prompt)이 뜬다.
   // - [x] 모달창에서 신규메뉴명을 입력 받고, 확인 버튼을 누르면 메뉴가 수정된다.
+  
+  // 이벤트 위임의 개념
   $('#espresso-menu-list').addEventListener('click',(event)=>{
     if(event.target.classList.contains('menu-edit-button')){
       const $menuName = event.target.closest("li").querySelector('.menu-name')
