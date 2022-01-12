40d6a626c31b09515dee088bf481cd284ce33606
Jay <63109802+bjw1622@users.noreply.github.com>
2022-01-12 16:03:53 +0900

    주석 수정

--- src/js/index.js
+++ src/js/index.js
@@ -64,9 +64,7 @@ const addMenu = () => {
 };
 
 function App() {
-    // TODO 메뉴 수정
-    // - [x] 메뉴의 수정은 button click event를 받고, 메뉴 수정하는 모달창(prompt)이 뜬다.
-    // - [x] 모달창에서 신규메뉴명을 입력 받고, 확인 버튼을 누르면 메뉴가 수정된다. 이벤트 위임의 개념
+    // 메뉴 수정
     $('#espresso-menu-list').addEventListener('click', (event) => {
         if (event.target.classList.contains('menu-edit-button')) {
             const $menuName = event
@@ -76,11 +74,7 @@ function App() {
             const updatedMenuName = prompt('메뉴명을 수정하세요', $menuName.innerText);
             $menuName.innerText = updatedMenuName;
         }
-        // TODO 메뉴 삭제
-        // - [] 메뉴의 삭제는 button click event를 받고, 메뉴 삭제 confirm 모달창이 뜬다.
-        // - [] 확인 버튼을 클릭하면 메뉴가 삭제된다.
-        // - [] 삭제 처리 후 총 메뉴 갯수를 count하여 상단에 보여준다. else if
-        // (event.target.classList.contains('menu-remove-button')) {
+    //  메뉴 삭제
         const index = confirm("메뉴를 정말로 삭제 하시겠습니끼?");
         if (index === true) {
             event
@@ -94,7 +88,9 @@ function App() {
                 console
                 .log(menuCount);
             $('.menu-count').innerText = `총 ${menuCount}개`
-
+        }
+        else{
+            console.log('아니요');
         }
     })
 
